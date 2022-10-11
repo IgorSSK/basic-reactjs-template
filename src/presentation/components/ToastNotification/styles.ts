c8ba9600df9c82c2bfd11e2c15ab8052fe5d5d44
Igor Souza <igors@DESKTOP-MO5U0SD.localdomain>
2022-10-10 23:54:20 -0300

    fix: Toast component text color

--- src/presentation/components/ToastNotification/styles.ts
+++ src/presentation/components/ToastNotification/styles.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
-import { ToastContainer as ToastifyContainer } from 'react-toastify'
+import { ToastContainer as ToastifyContainer } from 'react-toastify';
 
 export const ToastContainer = styled(ToastifyContainer)`
   .Toastify__toast-container {
   }
   .Toastify__toast {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  .Toastify__toast-body {
   }
   .Toastify__toast--error {
     background: ${({ theme }) => theme.colors.danger};
