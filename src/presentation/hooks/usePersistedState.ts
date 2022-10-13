ff501e40fc63637b18cf173242f12a8dd2d54317
Igor Souza <igors@DESKTOP-MO5U0SD.localdomain>
2022-10-13 19:29:09 -0300

    fix: added cookies as persisted option

--- src/presentation/hooks/usePersistedState.ts
+++ src/presentation/hooks/usePersistedState.ts
@@ -1,19 +1,44 @@
-import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction, useCallback } from 'react';
 
-type Response<T> = [T, Dispatch<SetStateAction<T>>];
+type Response<T> = [T, Dispatch<SetStateAction<T>>, () => void];
 
-function usePersistedState<T>(key: string, initialState: T): Response<T> {
+function usePersistedState<T>(
+  key: string,
+  initialState: T,
+  strategy: 'localStorage' | 'cookies' = 'localStorage'
+): Response<T> {
   const [state, setState] = useState(() => {
-    const storageValue = localStorage.getItem(key);
+    let storageValue = initialState;
+    if (strategy === 'localStorage') {
+      const item = localStorage.getItem(key);
+      if (item) storageValue = JSON.parse(item);
+    } else {
+      const item = document.cookie
+        .split(';')
+        ?.find(cookie => cookie.startsWith(key))
+        ?.split('=')
+        .at(1);
+      if (item) storageValue = JSON.parse(item);
+    }
 
-    return storageValue ? JSON.parse(storageValue) : initialState;
+    return storageValue;
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+    if (strategy === 'localStorage') localStorage.setItem(key, JSON.stringify(state));
+    else document.cookie = `${key}=${JSON.stringify(state)};SameSite=None;Secure`;
+  }, [key, state, strategy]);
 
-  return [state, setState];
+  const reset = useCallback(() => {
+    if (strategy === 'localStorage') localStorage.removeItem(key);
+    else document.cookie = `${key}=${JSON.stringify(state)};max-age=0`;
+
+    setState(initialState);
+  }, [key, state, strategy]);
+
+  // TODO: implement reset to clear key on storage
+
+  return [state, setState, reset];
 }
 
 export default usePersistedState;
